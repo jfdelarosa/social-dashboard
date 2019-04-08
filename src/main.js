71d78a3250ea262c4521f27d91f87bc1e14bdfad
Fernando <jfdelarosa@outlook.com>
2019-04-08 11:43:08 -0500

    Skip banned users

--- src/main.js
+++ src/main.js
@@ -8,36 +8,40 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import firebase from './firebase'
-import VueFire from 'vuefire'
+import { firestorePlugin } from 'vuefire'
 import axios from "axios"
 
-
 import ElementUI from 'element-ui'
-import lang from 'element-ui/lib/locale/lang/en'
-import locale from 'element-ui/lib/locale'
+import locale from 'element-ui/lib/locale/lang/en'
 
 import 'element-ui/lib/theme-chalk/index.css'
 import 'element-ui/lib/theme-chalk/display.css'
 import 'element-ui/lib/theme-chalk/reset.css'
 
-locale.use(lang)
-Vue.use(ElementUI)
+import sidebarLayout from './layouts/Sidebar'
+import simpleLayout from './layouts/Simple'
+import bulmaLayout from './layouts/Bulma'
 
+
+let baseURL
+if(process.env.NODE_ENV=="development"){
+  baseURL = "http://localhost:8888/social-api/"
+}else{
+  baseURL = "https://jf-social-api.herokuapp.com/"
+}
 let instance = axios.create({
-  baseURL: (process.env.NODE_ENV=="development" ? "http://localhost:8888/social-api/" : "https://jf-social-api.herokuapp.com/")
+  baseURL: baseURL
 })
 Vue.prototype.$http = instance
 
+
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-Vue.use(VueFire)
+Vue.use(ElementUI, { locale })
+Vue.use(firestorePlugin)
 
 library.add(fab)
 
 Vue.config.productionTip = false
-
-import sidebarLayout from './layouts/Sidebar'
-import simpleLayout from './layouts/Simple'
-import bulmaLayout from './layouts/Bulma'
 
 Vue.component("sidebar-layout", sidebarLayout)
 Vue.component("simple-layout", simpleLayout)
--- src/views/qp/Twitter.vue
+++ src/views/qp/Twitter.vue
@@ -2,7 +2,6 @@
   div
     div(style="margin-bottom: 1rem")
       el-date-picker(v-model="range" type="daterange" align="right" start-placeholder="Start Date" end-placeholder="End Date")
-      el-button(v-on:click="test") Test
       div(style="float: right")
         el-button(v-on:click="do_search" v-if="range.length == 2" icon="el-icon-search" type="success") Search
         el-button(v-on:click="showDialog = true" icon="el-icon-setting") Manage users
@@ -30,11 +29,11 @@
       el-table-column(prop="qpro" label="QuestionPro tweets" align="center")
 </template>
 <script>
-import firebase from '../../firebase'
-const db = firebase.firestore()
-export default{
-  data(){
-    return {
+import firebase from "../../firebase";
+const db = firebase.firestore();
+export default {
+  data() {
+    return {
       data: null,
       username: "",
       current: 0,
@@ -45,108 +44,106 @@ export default{
       status: "",
       showDialog: false,
       allUsers: []
-    }
+    };
   },
-  firestore(){
-    return {
+  firestore() {
+    return {
       allUsers: db.collection("qp_twitter")
-    }
+    };
   },
   computed: {
-    per(){
-      let per = (this.count.length / this.allUsers.length) * 100
-      return Number(per.toFixed(2))
+    per() {
+      let per = (this.count.length / this.allUsers.length) * 100;
+      return Number(per.toFixed(2));
     },
-    showBar(){
-      return (this.per >= 0 && this.per < 100) && this.showTable
+    showBar() {
+      return this.per >= 0 && this.per < 100 && this.showTable;
     }
   },
   methods: {
-    test(){
-      console.log(this.range);
-    },
-    add(){
+    add() {
       db.collection("qp_twitter").add({
         username: this.username.toLowerCase()
-      })
-      this.username = ""
+      });
+      this.username = "";
     },
-    remove(user){
-      db.collection('qp_twitter').where('username','==', user)
-      .get()
-      .then(function(snap){
-        snap.forEach(function(doc){
-          doc.ref.delete();
+    remove(user) {
+      db.collection("qp_twitter")
+        .where("username", "==", user)
+        .get()
+        .then(function(snap) {
+          snap.forEach(function(doc) {
+            doc.ref.delete();
+          });
+        });
+    },
+    is_loading(user) {
+      let isLoading = this.loading.indexOf(user) > -1;
+      return isLoading;
+    },
+    do_search() {
+      this.showTable = true;
+      this.count = [];
+      this.get_data();
+    },
+    get_data() {
+      let current = this.allUsers[this.current].username;
+      this.loading.push(current);
+      this.$http
+        .get("/twitter/tweets/" + current)
+        .then(res => {
+          let index = this.loading.indexOf(current);
+          this.loading.splice(index, 1);
+          if (!res.data.error) {
+            this.count_tweets(res.data, current);
+          }
+          this.current = this.current + 1;
+          if (this.current < this.allUsers.length) {
+            return this.get_data();
+          } else {
+            this.current = 0;
+            return "done";
+          }
         })
-      })
+        .catch(e => {
+          console.log(e);
+        });
     },
-    is_loading(user){
-      let isLoading = this.loading.indexOf(user) > -1
-      return isLoading
-    },
-    do_search(){
-      this.showTable = true
-      this.count = []
-      this.get_data()
-    },
-    check(){
-      console.log(this.count)
-    },
-    get_data(){
-      let current = this.allUsers[this.current].username
-      this.loading.push(current)
-      this.$http.get("/twitter/tweets/" + current)
-      .then(res => {
-        let index = this.loading.indexOf(current)
-        this.loading.splice(index, 1)
-        this.count_tweets(res.data, current)
-        this.current = this.current + 1
-        if(this.current < this.allUsers.length){
-          return this.get_data()
-        }else{
-          this.current = 0
-          return "done"
-        }
-      })
-      .catch(e => {
-        console.log(e)
-      })
-    },
-    count_tweets(data, user){
+    count_tweets(data, user) {
       let filterByDate = data.filter(tweet => {
-        let date = new Date(tweet.created_at)
-        let r1 = this.range[0]
-        let r2 = this.range[1]
-        let max = new Date(r2.toString())
-        
-        max.setHours(max.getHours() + 23)
-        max.setMinutes(max.getMinutes() + 59)
+        let date = new Date(tweet.created_at);
+        let r1 = this.range[0];
+        let r2 = this.range[1];
+        let max = new Date(r2.toString());
 
-        console.log(r1, max)
-        return date >= r1 && date <= max
-      })
+        max.setHours(max.getHours() + 23);
+        max.setMinutes(max.getMinutes() + 59);
+
+        console.log(r1, max);
+        return date >= r1 && date <= max;
+      });
 
       let filteredTweets = filterByDate.map(tweet => {
-        if(!tweet.entities){
-          return false
-        }else{        
+        if (!tweet.entities) {
+          return false;
+        } else {
           let filteredHashtags = tweet.entities.hashtags.filter(hash => {
-            return hash.text.toUpperCase() == "QUESTIONPRO"
-          })
-          return filteredHashtags
+            return hash.text.toUpperCase() == "QUESTIONPRO";
+          });
+          return filteredHashtags;
         }
-      })
-      
+      });
+
       let QP = filteredTweets.filter(tweet => {
-        return tweet.length > 0
-      })
+        return tweet.length > 0;
+      });
 
       this.count.push({
         username: user,
         free: filteredTweets.length - QP.length,
         qpro: QP.length
-      })
+      });
     }
   }
-}
+};
 </script>
